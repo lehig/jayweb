3e8f40316ca44c1c8fecfb033d8e1c20829a26cb
lehig <97483848+lehig@users.noreply.github.com>
2022-07-28 16:24:29 -0600

    added filter in the family document and added a javascript file

---
+++ src/script.js
@@ -0,0 +1,8 @@
+// var part1 = "user";
+// var part2 = Math.pow(2, 6);
+// var part3 = String.fromCharCode(part2);
+// var part4 = "domain.com";
+// var part5 = part1 + String.fromCharCode(part2) + part4;
+// var class_ = "nav-link";
+document.getElementById("email-link").innerHTML =
+  '<a href="mailto:inf@example.com">Email</a>.';
