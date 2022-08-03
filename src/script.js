2b6ba6f95da0c3307dd5c36b98fe69e1ce276f6a
lehig <97483848+lehig@users.noreply.github.com>
2022-08-03 13:49:06 -0600

    javascript added for the modal contact form

--- src/script.js
+++ src/script.js
@@ -1,8 +1,20 @@
-// var part1 = "user";
-// var part2 = Math.pow(2, 6);
-// var part3 = String.fromCharCode(part2);
-// var part4 = "domain.com";
-// var part5 = part1 + String.fromCharCode(part2) + part4;
-// var class_ = "nav-link";
-document.getElementById("email-link").innerHTML =
-  '<a href="mailto:inf@example.com">Email</a>.';
+(function () {
+  emailjs.init("dWgwfT7lNtveSBNgz");
+})();
+
+window.onload = function () {
+  document
+    .getElementById("modal-body")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      this.contact_number.value = (Math.random() * 100000) | 0;
+      emailjs.sendForm("contact_service", "contact_form", this).then(
+        function () {
+          console.log("SUCCESS!");
+        },
+        function (error) {
+          console.log("FAILED...", error);
+        }
+      );
+    });
+};
