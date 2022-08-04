39a0d9bc91aba10fa10084154b6abb9220a5410d
lehig <97483848+lehig@users.noreply.github.com>
2022-08-04 14:07:52 -0600

    the email me feature finally works

--- src/script.js
+++ src/script.js
@@ -7,10 +7,8 @@ window.onload = function () {
     .getElementById("contact-form")
     .addEventListener("submit", function (event) {
       event.preventDefault();
-      // generate a five digit number for the contact_number variable
-      this.contact_number.value = (Math.random() * 100000) | 0;
       // these IDs from the previous steps
-      emailjs.sendForm("contact_service", "contact_form", this).then(
+      emailjs.sendForm("service_9h4cxed", "template_bh2armk", this).then(
         function () {
           console.log("SUCCESS!");
         },
