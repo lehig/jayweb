42cb65fcbd0426b23c00dc29d5dbd5a771757f10
lehig <97483848+lehig@users.noreply.github.com>
2022-08-04 13:38:36 -0600

    javascript reworked and edited to work... hopefully

--- src/script.js
+++ src/script.js
@@ -4,10 +4,12 @@
 
 window.onload = function () {
   document
-    .getElementById("modal-body")
+    .getElementById("contact-form")
     .addEventListener("submit", function (event) {
       event.preventDefault();
+      // generate a five digit number for the contact_number variable
       this.contact_number.value = (Math.random() * 100000) | 0;
+      // these IDs from the previous steps
       emailjs.sendForm("contact_service", "contact_form", this).then(
         function () {
           console.log("SUCCESS!");
